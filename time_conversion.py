c44b7b775694eb8ed2556aac1e5a16d637a8792d
manjushree581 <manjushreepb14@gmail.com>
2024-12-10 16:29:31 +0530

    Create time_conversion.py

---
+++ time_conversion.py
@@ -0,0 +1,12 @@
+a=int(input("enter minutes "))
+hours=int(a)/60
+print(hours )
+minutes=int(a)%60
+print(minutes )
+c=a//b
+print(c )
+
+minutes=int(input("enter time in minutes "))
+hours=minutes//60
+b=minutes%60
+print(f"time: {hours} hours and {b} minutes")
