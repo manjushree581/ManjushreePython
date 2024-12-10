b2200152971be59f0243121a7ec957652e2109c9
manjushree581 <manjushreepb14@gmail.com>
2024-12-10 16:13:40 +0530

    Create area_and_perimeter_rectangle.py

---
+++ area_and_perimeter_rectangle.py
@@ -0,0 +1,6 @@
+l=float(input("enter the length "))
+b=float(input("enter the breadth"))
+area=l*b
+perimeter=2*(l+b)
+print("the area of rectangle is" , area)
+print("the perimeter of rectangle is" , perimeter)
